c51288d6bef899c2caca16aa389d32b0a0766b52
uppara <upparavinay2003n@gmail.com>
2025-09-18 17:28:09 +0530

    basic code &versionlock

---
+++ Day2-basiccode-versionlock/variables.tf
@@ -0,0 +1,20 @@
+variable "ami" {
+    description = "value"
+    type = string
+    default = ""
+  
+}
+
+variable "type" {
+    description = "value"
+    type = string
+    default = ""
+  
+}
+
+variable "key_name" {
+    description = "value"
+    type = string
+    default = ""
+  
+}
